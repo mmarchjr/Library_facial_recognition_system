d1b1ac1039b399d2b4ffea0367f2730e459e118d
MMarchjr <103056947+mmarchjr@users.noreply.github.com>
2022-11-09 10:24:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
     
-    const URL = prompt("enter the ml url", 
+    const URL = prompt("enter the ml url", "https://teachablemachine.withgoogle.com/models/g_kytcN23/" 
  );//"https://teachablemachine.withgoogle.com/models/g_kytcN23/";
 
     let model, webcam, labelContainer, maxPredictions;
